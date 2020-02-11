8f81eeae95ee388f09534e81d407e90ff8568811
Daba Dashiev <dabakrytoe@yandex.ru>
2020-02-10 19:05:42 -0500

    adding inex for heroku

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Lab-5</title>
+</head>
+<body>
+    <header>
+         <h1>Lab05</h1> 
+    </header>
+<a href="./lab/table.php">Table of products</a>
+
+</body>
+</html>
